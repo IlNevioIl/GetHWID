71c45ce56d6dafbc8ff30155db412643584acbd8
IlNevioIl <Nevio.vogt@gmail.com>
2021-04-08 12:45:04 +0200

    rewrote

--- src/me/ilnevioil/main/Main.java
+++ src/me/ilnevioil/main/Main.java
@@ -11,7 +11,10 @@ public class Main {
     public static String hwid = System.getenv("os") + System.getProperty("os.name") + System.getProperty("os.arch") + System.getProperty("os.version") + System.getProperty("user.language") + System.getenv("SystemRoot") + System.getenv("HOMEDRIVE") + System.getenv("PROCESSOR_LEVEL") + System.getenv("PROCESSOR_REVISION") + System.getenv("PROCESSOR_IDENTIFIER") + System.getenv("PROCESSOR_ARCHITECTURE") + System.getenv("PROCESSOR_ARCHITEW6432") + System.getenv("NUMBER_OF_PROCESSORS");
     public static void main(String args[]) {
 
+        System.out.println("Your HWID is: " + hwid);
+
         //Todo: Gui
+        //All this down here is for a future gui update:
 
         /*JFrame jf1 = new JFrame();
         jf1.setVisible(true);
@@ -27,8 +30,6 @@ public class Main {
         jLabel.setVisible(true);
         jLabel.setBounds(150, 20, 200, 20);
         jf1.add(jLabel);*/
-
-        System.out.println("Your HWID is: " + hwid);
 
     }
 }
