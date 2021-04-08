a6c4818b07ab02d176bcdf985c7ab5f9f61efb8f
IlNevioIl <36001296+IlNevioIl@users.noreply.github.com>
2021-04-08 03:08:36 +0200

    Add files via upload

---
+++ src/me/ilnevioil/main/Main.java
@@ -0,0 +1,34 @@
+package me.ilnevioil.main;
+
+import javax.swing.*;
+
+/**
+ * Made by _bt on 08 April 2021
+ */
+
+public class Main {
+
+    public static String hwid = System.getenv("os") + System.getProperty("os.name") + System.getProperty("os.arch") + System.getProperty("os.version") + System.getProperty("user.language") + System.getenv("SystemRoot") + System.getenv("HOMEDRIVE") + System.getenv("PROCESSOR_LEVEL") + System.getenv("PROCESSOR_REVISION") + System.getenv("PROCESSOR_IDENTIFIER") + System.getenv("PROCESSOR_ARCHITECTURE") + System.getenv("PROCESSOR_ARCHITEW6432") + System.getenv("NUMBER_OF_PROCESSORS");
+    public static void main(String args[]) {
+
+        //Todo: Gui
+
+        /*JFrame jf1 = new JFrame();
+        jf1.setVisible(true);
+        jf1.setSize(500, 100);
+        jf1.setTitle("HWID Scanner by Nevio");
+        jf1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf1.setLayout(null);
+        jf1.setResizable(false);
+        jf1.setLocationRelativeTo(null);
+        jf1.requestFocus();
+
+        JLabel jLabel = new JLabel("==Please use this in your cmd==");
+        jLabel.setVisible(true);
+        jLabel.setBounds(150, 20, 200, 20);
+        jf1.add(jLabel);*/
+
+        System.out.println("Your HWID is: " + hwid);
+
+    }
+}
