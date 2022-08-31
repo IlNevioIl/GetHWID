ab15cf0974738dbfe1f65dfec1d202f194c91d91
Peter <pedrohenriquebm1108@gmail.com>
2022-08-31 13:25:40 -0300

    JOptionPane message added for now!

--- src/me/ilnevioil/main/Main.java
+++ src/me/ilnevioil/main/Main.java
@@ -4,18 +4,17 @@ import javax.swing.*;
 
 /**
  * Made by _bt on 08 April 2021
+ * JOptionPane message added by Peter on 31 August 2022
  */
 
 public class Main {
-
     public static String hwid = System.getProperty("user.name") + " " + System.getenv("COMPUTERNAME") + " " + System.getenv("os") + " " + System.getProperty("os.name") + " " + System.getenv("PROCESSOR_IDENTIFIER") + " " + System.getProperty("os.arch") + " " + System.getProperty("os.version") + " " + System.getProperty("user.language") + " " + System.getenv("SystemRoot") + " " + System.getenv("HOMEDRIVE") + " " + System.getenv("PROCESSOR_LEVEL") + " " + System.getenv("PROCESSOR_REVISION") + " " + System.getenv("PROCESSOR_IDENTIFIER") + " " + System.getenv("PROCESSOR_ARCHITECTURE") + " " + System.getenv("PROCESSOR_ARCHITEW6432") + " " + System.getenv("NUMBER_OF_PROCESSORS");
 
     public static void main(String args[]) {
-
-        System.out.println("Your HWID is: " + hwid);
+        showMessage(); //Show the JOptionPane message! - Peter
 
         //Todo: Gui
-        //All this down here is for a future gui update:
+        //All this down here is for a future gui update (there will be a JOptionPane message for now - Peter):
 
         /*JFrame jf1 = new JFrame();
         jf1.setVisible(true);
@@ -31,6 +30,16 @@ public class Main {
         jLabel.setVisible(true);
         jLabel.setBounds(150, 20, 200, 20);
         jf1.add(jLabel);*/
+    }
 
+    public static void showMessage() {
+        copyToClipboard();
+        JOptionPane.showMessageDialog((Component)null, "HWID: " + hwid, "Copied to clipboard!", 0);
+    }
+
+    public static void copyToClipboard() {
+        StringSelection selection = new StringSelection(hwid);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(selection, selection);
     }
 }
